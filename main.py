756f1f0f79557bb570fde34014a382d23cadf4d8
Stefan Helmert <stefan.helmert@t-online.de>
2024-11-08 13:44:08 +0100

    auto reconnect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import scrolledtext
 import serial
+from serial.tools import list_ports
 import threading
 import time
 
@@ -9,6 +10,7 @@ class SerialApp:
         self.root = root
         self.root.title("Serial Data Logger")
 
+        self.serial_port = None
         # Frame für die Textfelder und den Button
         frame = tk.Frame(root)
         frame.pack(pady=10)
@@ -27,6 +29,7 @@ class SerialApp:
         self.button = tk.Button(frame, text="Snapshot speichern", command=self.save_snapshot)
         self.button.pack(pady=10)
 
+        self.serial_port_name = 'COM9'
         self.setup_serial()
 
         # Fenster schließen und seriellen Port freigeben
@@ -42,35 +45,50 @@ class SerialApp:
             self.snapshot_log.see(tk.END)  # Zum Ende des Logs scrollen
 
     def setup_serial(self):
+        if self.serial_port is None:
+            self.open_serial(self.serial_port_name)
+        elif not self.serial_port.is_open:
+            self.open_serial(self.serial_port_name)
+        elif self.serial_port.port not in list_ports.comports():
+            self.serial_port = None
+        self.root.after(500,self.setup_serial)
+
+
+    def open_serial(self, port):
         # Serielle Schnittstelle initialisieren
         try:
             # Seriellen Port auf COM9 und Baudrate auf 115200 setzen
-            self.serial_port = serial.Serial('COM9', baudrate=115200, timeout=1)
+            self.serial_port = serial.Serial(port, baudrate=115200, timeout=1)
             print("Verbindung zur seriellen Schnittstelle hergestellt.")
         except serial.SerialException as e:
             print("Fehler beim Öffnen der seriellen Schnittstelle:", e)
             self.serial_port = None
 
+    def close_serial(self):
+        if self.serial_port:
+            self.serial_port.close()  # Schließt die serielle Schnittstelle
 
     def read_serial_data(self):
-        while self.serial_port and self.serial_port.in_waiting > 0:
-                try:
-                    # Empfange und dekodiere die aktuelle Zeile
-                    #print("before")
-                    c = self.serial_port.read()
-                    #print("after")
-                    if c != b'\n':
-                        self.buffer += c
-                        continue
-                    data = self.buffer.decode('utf-8').strip()
-                    self.buffer = b''
-                    if data:
-                        # Zeige die aktuelle empfangene Zeile in der Live-Anzeige an
-                        self.live_display.delete("1.0", tk.END)  # Löscht den alten Inhalt
-                        self.live_display.insert(tk.END, data)  # Fügt neuen Inhalt ein
-                except Exception as e:
-                    print("Fehler beim Lesen der Daten:", e)
-                    break
+        try:
+            while self.serial_port and self.serial_port.in_waiting > 0:
+                # Empfange und dekodiere die aktuelle Zeile
+                #print("before")
+                c = self.serial_port.read()
+                #print("after")
+                if c != b'\n':
+                    self.buffer += c
+                    continue
+                data = self.buffer.decode('utf-8').strip()
+                self.buffer = b''
+                if data:
+                    # Zeige die aktuelle empfangene Zeile in der Live-Anzeige an
+                    self.live_display.delete("1.0", tk.END)  # Löscht den alten Inhalt
+                    self.live_display.insert(tk.END, data)  # Fügt neuen Inhalt ein
+        except Exception as e:
+            print("Fehler beim Lesen der Daten:", e)
+            self.buffer = b''
+            #self.serial_port.close()
+                    
         self.root.after(100, self.read_serial_data)
 
 
@@ -78,8 +96,7 @@ class SerialApp:
         # Methode zum Schließen des Fensters und Beenden des Threads
         print("Schließe Anwendung...")
         # Warte, bis der Thread beendet ist
-        if self.serial_port:
-            self.serial_port.close()  # Schließt die serielle Schnittstelle
+        self.close_serial()
         self.root.destroy()  # Beendet das Tkinter-Hauptfenster
 
 # Tkinter-Root-Fenster erstellen
